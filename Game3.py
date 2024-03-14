d46f7fe5f115fb6b48d0d657ac74548347c385a6
sugiyama <sugiyama@curantace.co.jp>
2024-03-14 21:30:08 +0900

    2024.03.14 sugiyama

--- Game3.py
+++ Game3.py
@@ -5,16 +5,27 @@ from djitellopy import Tello
 import time
 import random
 
-# 定数
-FIRST_LIMIT_TIME = 120 - 30     # 自由に動ける時間（期限）
-SECOND_LIMIT_TIME = 180 - 30    # 上下のみ移動できる時間（期限）
-START_Z_POS = 100               # 最初の高さ
-FIRST_XY_MIN = 10               # 最初の移動下限
-FIRST_XY_MAX = 20               # 最初の移動上限
+# 定数（テスト用）
+# FIRST_LIMIT_TIME = 120 - 30     # 自由に動ける時間（期限）
+# SECOND_LIMIT_TIME = 180 - 30    # 上下のみ移動できる時間（期限）
+# START_Z_POS = 100               # 最初の高さ
+# FIRST_XY_MIN = 0               # 最初の移動下限
+# FIRST_XY_MAX = 0               # 最初の移動上限
+# XY_MIN = 5                      # 平面移動下限
+# XY_MAX = 10                     # 平面移動上限
+# Z_MIN = 3                       # 上下移動下限
+# Z_MAX = 6                       # 上下移動上限
+
+# 定数（本番用）
+FIRST_LIMIT_TIME = 300 - 30     # 自由に動ける時間（期限）
+SECOND_LIMIT_TIME = 480 - 30    # 上下のみ移動できる時間（期限）
+START_Z_POS = 200               # 最初の高さ
+FIRST_XY_MIN = 35               # 最初の移動下限
+FIRST_XY_MAX = 55               # 最初の移動上限
 XY_MIN = 5                      # 平面移動下限
-XY_MAX = 10                     # 平面移動上限
+XY_MAX = 30                     # 平面移動上限
 Z_MIN = 3                       # 上下移動下限
-Z_MAX = 6                      # 上下移動上限
+Z_MAX = 12                      # 上下移動上限
 
 
 def main():
@@ -29,7 +40,7 @@ def main():
     x_move = random.randint(FIRST_XY_MIN,FIRST_XY_MAX) * 10
     y_move = random.randint(FIRST_XY_MIN,FIRST_XY_MAX) * 10
     z_move = START_Z_POS - tello.get_height()
-    move_speed = random.randint(4,8)
+    move_speed = random.randint(30,80)
     tello.go_xyz_speed(x_move, y_move, z_move, move_speed)
 
     # 決められた時間まで自由に動ける
@@ -40,55 +51,55 @@ def main():
         z_move = random.randint(Z_MIN, Z_MAX) * 10
 
         # 平行移動１
-        move_speed = random.randint(4,8)
+        move_speed = random.randint(30,80)
         tello.go_xyz_speed(x_move, y_move, 0, move_speed)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
         # 上下移動１
         tello.move_up(z_move)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
 
         # 平行移動２
-        move_speed = random.randint(4,8)
+        move_speed = random.randint(30,80)
         tello.go_xyz_speed(x_move * -2, y_move * -2, 0, move_speed)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
         # 上下移動２
-        tello.move_down(z_move * -2)
+        tello.move_down(z_move * 2)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
 
         # 平行移動３
-        move_speed = random.randint(4,8)
+        move_speed = random.randint(30,80)
         tello.go_xyz_speed(x_move, y_move, 0, move_speed)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
         # 上下移動３
         tello.move_up(z_move)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
 
         current_time = time.time()
 
     while current_time - start_time < SECOND_LIMIT_TIME:
         z_move = random.randint(Z_MIN, Z_MAX) * 10
-        move_speed = random.randint(4,8)
+        move_speed = random.randint(30,80)
 
         # 上下移動１
         tello.move_up(z_move)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
 
         # 上下移動２
-        tello.move_down(z_move * -2)
+        tello.move_down(z_move * 2)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
 
         # 上下移動３
         tello.move_up(z_move)
         # 待機
-        time.sleep(move_speed * 0.2)
+        time.sleep(move_speed * 0.04)
 
         current_time = time.time()
 
