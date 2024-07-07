63b31537d3b7e586788b085ee885e0fbccb96fc9
watanabe-crn <147138500+watanabe-crn@users.noreply.github.com>
2024-07-07 23:19:46 +0900

    20240707_渡邊

--- acctello.py
+++ acctello.py
@@ -257,38 +257,40 @@ class AccTello:
 
 
     def image_analysis(self, image):
+        # 拡縮（閾値調整用）
+        # 30cm:3 50cm:1 100cm:0.3(距離によってscalingの数字を調整)
+        scaling = 1
+
+        # ノイズ除去閾値
+        contourArea_min = 500*scaling
+        contourArea_max = 1000*scaling
+
+        ## 平均化するピクセル数
+        #pixel_a = 30
+        #pixel_b = 30
+
         # 色基準で2値化する。
-    #    image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
         # 閾値の設定
-        threshold = 10
+        threshold = 100
 
         # 二値化(閾値を超えた画素を255にする。)
         ret, img_thresh = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY_INV)
 
         contours, hierarchy= cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        #デバッグ用
-        #cv2.imwrite('output_shapes1.png',img_thresh)
-
         #特定の面積以外のノイズ除去 ここの数字を適切な値にする
-        cv2.drawContours(img_thresh,[i for i in contours if abs(cv2.contourArea(i))<= 130 or abs(cv2.contourArea(i))> 170], -1,(0,0,0),-1)
+        cv2.drawContours(img_thresh,[i for i in contours if abs(cv2.contourArea(i))<= contourArea_min or abs(cv2.contourArea(i))> contourArea_max], -1,(0,0,0),-1)
 
         #デバッグ用
-        #cv2.imwrite('output_shapes2.png',img_thresh)
+        cv2.imwrite('output_shapes1.png',img_thresh)
 
         #第一引数で輝度で平均化処理する。
         #第二引数は平均化するピクセル数で30x30ピクセル
-        img_thresh = cv2.blur(img_thresh,(3,3))
-
-        #デバッグ用
-        #cv2.imwrite('output_shapes3.png',img_thresh)
+    #    img_thresh = cv2.blur(img_thresh,(pixel_a,pixel_b))
 
         contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #輪郭抽出
-
-        #デバッグ用
-        #cv2.imwrite('output_shapes4.png',img_thresh)
 
         count_objects_image = len(contours)
 
